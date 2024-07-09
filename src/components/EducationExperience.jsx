25eed8e4db1214c58318a9e7ae0320f5bb071353
futurebound <mitchellm1618@gmail.com>
2024-07-09 09:40:07 -0700

    feat: EducationExperience subfields added
    School name, field of study and graduation date with appropriate input types

--- src/components/EducationExperience.jsx
+++ src/components/EducationExperience.jsx
@@ -5,6 +5,12 @@ function EducationExperience({ type }) {
 	return (
 		<div>
 			{type} Experience Section
+			<p>School Name</p>
+			<input type="text" />
+			<p>Field of Study</p>
+			<input type="text" />
+			<p>Graduation Date</p>
+			<input type="date" />
 		</div>
 	)
 }
