3467229eb87c4189221015bbcb5d683e8f0a5282
futurebound <mitchellm1618@gmail.com>
2024-07-09 09:41:50 -0700

    feat: WorkExperience input fields
    Added relevant work experience input fields

--- src/components/WorkExperience.jsx
+++ src/components/WorkExperience.jsx
@@ -5,6 +5,16 @@ function WorkExperience({ type }) {
 	return (
 		<div>
 			{type} Experience Section
+			<p>Company Name</p>
+			<input type="text" />
+			<p>Position</p>
+			<input type="text" />
+			<p>Responsibilities</p>
+			<input type="text" />
+			<p>Start Date</p>
+			<input type="date" />
+			<p>End Date</p>
+			<input type="date" />
 		</div>
 	)
 }
